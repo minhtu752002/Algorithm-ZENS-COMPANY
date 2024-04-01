7dd33ec0d23ee6badd09382f2f154a9d359e112d
minhtu752002 <20520836@gm.uit.edu.vn>
2024-04-01 17:45:56 +0700

    init Algorithm

---
+++ MiniMaxSum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+	static void MiniMaxSum(int[] arr)
+	{
+		Array.Sort(arr);
+		long minSum = 0, maxSum = 0;
+
+		for (int i = 0; i < 4; ++i)
+		{
+			minSum += arr[i];
+		}
+
+		for (int i = 1; i < 5; ++i)
+		{
+			maxSum += arr[i];
+		}
+
+		Console.WriteLine($"{minSum} {maxSum}");
+
+		for (int i = 0; i < 5; ++i)
+		{
+			long sum = 0;
+			for (int j = 0; j < 5; ++j)
+			{
+				if (j != i)
+				{
+					sum += arr[j];
+				}
+			}
+			Console.WriteLine($"If we sum everything except {arr[i]}, our sum is: {sum}");
+		}
+	}
+
+	static void Main(string[] args)
+	{
+		int[] arr = new int[5];
+		Console.WriteLine("Enter 5 integers separated by spaces: ");
+		string input = Console.ReadLine();
+		string[] inputArr = input.Split(' ');
+		for (int i = 0; i < 5; ++i)
+		{
+			arr[i] = int.Parse(inputArr[i]);
+		}
+		MiniMaxSum(arr);
+	}
+}
